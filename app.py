42fad77721c14b3a33b3f3cd85e5a8b4e1be3333
kayuri03 <marcelokaneshigue@gmail.com>
2026-04-16 10:40:50 -0400

    Prepare for Render deployment: add Procfile, update requirements, disable debug mode

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ from todo_app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
